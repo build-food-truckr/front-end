b7dbe21daa52462ffab5a61d0c65f0138f558032
bobbidigi <bobbidigi34@gmail.com>
2020-03-25 01:08:26 -0400

    add config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// console.log({
+//     secret: process.env.secret,
+//     id: process.env.id,
+// })
+
+module.exports = {
+    env: {
+      secret: "J52RRBCMCEAY4HOIEZGXTDKZGKFA13NWKSWASG0WJAWSCHZ0",
+      id: "L0ZK3VLIPIYJBRFHR2D421ZUPVBOEBUPMR5PQFKWDMT1UQJA",
+      key: "pk.eyJ1IjoiYm9iYmlkaWdpIiwiYSI6ImNrN2pmMGZuYTAzY2MzbmxtbzRyYzk0amEifQ._rAmLPNij_hAnD-R7R3RmQ"
+    },
+}
